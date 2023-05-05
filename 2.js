60308353cd81cb5246238032b8f4cab0ccf3d025
sa1kri <92201506+sa1kri@users.noreply.github.com>
2023-05-05 22:24:54 +0500

    Add files via upload

--- 2.js
+++ 2.js
@@ -1,2 +1,14 @@
-a, b = int(input()), int(input());
-print(a / b, a % b);
+let candiesPerDay = [];
+
+for (let  i = 1; i <=7; i++) {
+const konfet = prompt("сколько конфет съела в день ", {i}, "?");
+candiesPerDay.push(Number(konfet));
+};
+
+let  sum = 0;
+for (let i = 0; i < candiesPerDay.length; i++) {
+sum += candiesPerDay[i];
+};
+
+const average = sum / candiesPerDay.length;
+console.log("За последние 7 дней ты съела", sum, "конфет. Среднее количество конфет в день: ", average);
